dd397cc3052b5eb547c4b215424931056af845f7
Quentin Beauvais <quentin.beauvais@hec.edu>
2017-03-13 19:13:11 +0100

    cloudinary

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
 
     resources :users, only: [:edit, :update, :show]
 
+    mount Attachinary::Engine => "/attachinary"
+
     # get '/profile', to: 'pages#about'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
