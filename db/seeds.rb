9309cbe906bf5b080f0baa73ba82decefba75cfe
Simon De Swarte <simon.de-swarte@hotmail.fr>
2017-03-14 16:40:03 +0100

    Index styl updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+n = 0
+20.times do
+ n +=1
+ user = User.new(
+   first_name: Faker::Name.first_name,
+   last_name: Faker::Name.last_name,
+   email: Faker::Internet.email,
+   birthday: Random.rand(365),
+   description: Faker::ChuckNorris.fact,
+   password: "zzzzzz"
+   )
+ user.save!
+
+ url = "http://lorempixel.com/1000/1000/nature"
+ plant = Plant.new(
+   name: Faker::Pokemon.name,
+   price: Random.rand(100),
+   height: Random.rand(500),
+   location: Faker::Pokemon.location,
+   description: Faker::StarWars.quote,
+   user_id: n
+   )
+ plant.photo_url = url
+ plant.save!
+end
