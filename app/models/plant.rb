5a7873291988907e5ec2b216153d9f20e62600ff
Florian Stegre <florian@stegre.com>
2017-03-14 15:00:18 +0100

    problems solved with the forms to create a plant

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -5,6 +5,6 @@ class Plant < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :description, presence: true
   validates :price, presence: true
-  validates :height, presence: true, numericality: true
+  validates :height, numericality: true, allow_blank: true
   validates :photo, presence: true
 end
