b238268ff4fb267cebb400615d6fe315dc12729b
Simon De Swarte <simon.de-swarte@hotmail.fr>
2017-03-16 14:44:34 +0100

    Rocketing user profile page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,3 +8,16 @@
 //= require underscore
 //= require gmaps/google
 
+$(function(){
+
+  $(".tab").on("click", function(e){
+    // Change active tab
+    $(".tab").removeClass('active');
+    $(this).addClass('active');
+    // Hide all tab-content (use class="hidden")
+    $(".tab-content").addClass('hidden');
+    // Show target tab-content (use class="hidden")
+    $($(this).data('target')).removeClass('hidden');
+  });
+
+});
