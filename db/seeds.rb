4ef9f707b7feee4e2bc578aeb640af34ccc58a2c
Florian Stegre <florian@stegre.com>
2017-03-17 11:26:01 +0100

    seed modifs

--- db/seeds.rb
+++ db/seeds.rb
@@ -39,4 +39,32 @@ User.destroy_all
   plant.save!
 end
 
+5.times do
+  url = "https://source.unsplash.com/collection/580685"
+  sleep 1
+  user = User.new(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    birthday: Random.rand(365),
+    description: Faker::ChuckNorris.fact,
+    password: "zzzzzz"
+    )
+  user.photo_url = url
+  user.save!
+
+  url = "https://source.unsplash.com/collection/404339"
+  sleep 1
+  plant = Plant.new(
+    name: Faker::Pokemon.name,
+    price: Random.rand(100),
+    height: Random.rand(500),
+    location: "Paris",
+    description: Faker::StarWars.quote,
+    user_id: User.all.last.id
+    )
+  plant.photo_url = url
+  plant.save!
+end
+
 puts "Seed OK"
